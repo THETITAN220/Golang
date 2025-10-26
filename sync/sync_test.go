446fed04ee7e11ca9eb23732cfaaccdbfe9f9015
THETITAN220 <raunakrose15@gmail.com>
2025-10-26 21:09:34 +0530

    sync complete

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -5,10 +5,14 @@ import (
 	"testing"
 )
 
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
 func TestCounter(t *testing.T) {
 
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
-		counter := Counter{}
+		counter := NewCounter()
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
@@ -19,7 +23,7 @@ func TestCounter(t *testing.T) {
 	t.Run("it runs safe concurrently", func(t *testing.T) {
 		wantedCounter := 1000
 
-		counter := Counter{}
+		counter := NewCounter()
 
 		var wg sync.WaitGroup
 		wg.Add(wantedCounter)
@@ -36,7 +40,7 @@ func TestCounter(t *testing.T) {
 	})
 }
 
-func assertCounter(t testing.TB, got Counter, want int) {
+func assertCounter(t testing.TB, got *Counter, want int) {
 	t.Helper()
 	if got.Value() != want {
 		t.Errorf("got %d, want %d", got.Value(), want)
