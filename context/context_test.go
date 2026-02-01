1569206a8f9b81300b1c1ac240a0476f9ca8e01d
THETITAN220 <raunakrose15@gmail.com>
2026-02-01 12:25:09 +0530

    more stuff

--- context/context_test.go
+++ context/context_test.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 )
@@ -36,7 +37,7 @@ func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
 	data := make(chan string, 1)
 
 	go func() {
-		var result string
+		var result strings.Builder
 		for _, c := range s.response {
 			select {
 			case <-ctx.Done():
@@ -44,10 +45,10 @@ func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
 				return
 			default:
 				time.Sleep(time.Millisecond * 10)
-				result += string(c)
+				result.WriteString(string(c))
 			}
 		}
-		data <- result
+		data <- result.String()
 	}()
 
 	select {
